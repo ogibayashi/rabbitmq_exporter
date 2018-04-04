39965b5f4c40d2ef1b54c0bf7e240b814233302a
Hironori Ogibayashi <hironori.ogibayashi@linecorp.com>
2018-04-04 15:24:35 +0900

    Add message_stats from overview

--- exporter_overview.go
+++ exporter_overview.go
@@ -10,14 +10,26 @@ func init() {
 }
 
 var overviewMetricDescription = map[string]prometheus.Gauge{
-	"object_totals.channels":               newGauge("channelsTotal", "Total number of open channels."),
-	"object_totals.connections":            newGauge("connectionsTotal", "Total number of open connections."),
-	"object_totals.consumers":              newGauge("consumersTotal", "Total number of message consumers."),
-	"object_totals.queues":                 newGauge("queuesTotal", "Total number of queues in use."),
-	"object_totals.exchanges":              newGauge("exchangesTotal", "Total number of exchanges in use."),
-	"queue_totals.messages":                newGauge("queue_messages_total", "Total number ready and unacknowledged messages in cluster."),
-	"queue_totals.messages_ready":          newGauge("queue_messages_ready_total", "Total number of messages ready to be delivered to clients."),
-	"queue_totals.messages_unacknowledged": newGauge("queue_messages_unacknowledged_total", "Total number of messages delivered to clients but not yet acknowledged."),
+	"object_totals.channels":                       newGauge("channelsTotal", "Total number of open channels."),
+	"object_totals.connections":                    newGauge("connectionsTotal", "Total number of open connections."),
+	"object_totals.consumers":                      newGauge("consumersTotal", "Total number of message consumers."),
+	"object_totals.queues":                         newGauge("queuesTotal", "Total number of queues in use."),
+	"object_totals.exchanges":                      newGauge("exchangesTotal", "Total number of exchanges in use."),
+	"queue_totals.messages":                        newGauge("queue_messages_total", "Total number ready and unacknowledged messages in cluster."),
+	"queue_totals.messages_ready":                  newGauge("queue_messages_ready_total", "Total number of messages ready to be delivered to clients."),
+	"queue_totals.messages_unacknowledged":         newGauge("queue_messages_unacknowledged_total", "Total number of messages delivered to clients but not yet acknowledged."),
+	"message_stats.publish_details.rate":           newGauge("message_publish_rate", "Message rate of messages published."),
+	"message_stats.confirm_details.rate":           newGauge("message_confirm_rate", "Message rate of messages confirmed."),
+	"message_stats.return_unroutable_details.rate": newGauge("message_return_rate", "Message rate of messages returned to publisher as unroutable."),
+	"message_stats.disk_reads_details.rate":        newGauge("message_disk_reads_rate", "Message rate of messages have been read from disk."),
+	"message_stats.disk_writes_details.rate":       newGauge("message_disk_writes_rate", "Message rate of messages have been written to disk."),
+	"message_stats.get_details.rate":               newGauge("message_get_rate", "Message rate of messages delivered in acknowledgement mode in response to basic.get."),
+	"message_stats.get_no_ack_details.rate":        newGauge("message_get_no_ack_rate", "Message rate of messages delivered in no-acknowledgement mode in response to basic.get."),
+	"message_stats.deliver_details.rate":           newGauge("message_deliver_rate", "Message rate of messages delivered in acknowledgement mode to consumers."),
+	"message_stats.deliver_no_ack_details.rate":    newGauge("message_deliver_no_ack_rate", "Message rate of messages delivered in no-acknowledgement mode to consumers."),
+	"message_stats.redeliver_details.rate":         newGauge("message_redeliver_rate", "Message rate of messages in deliver_get which had the redelivered flag set."),
+	"message_stats.ack_details.rate":               newGauge("message_ack_rate", "Message rate of ack."),
+	"message_stats.deliver_get_details.rate":       newGauge("message_deliver_get_rate", "Message rate of sum of deliver/get."),
 }
 
 type exporterOverview struct {
